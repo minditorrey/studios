7617e84266a89f971d8e40f2442c799a0da1884f
Mindi Torrey <minditorrey@me.com>
2016-10-01 10:53:56 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(function() {
+	//cache window object
+	var $window = $(window);
+
+	//parallax effect
+
+	$('section[data-type="background"]').each(function() {
+		
+		var $bgobj = $(this);
+
+		$(window).scroll(function() {
+
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+			var coords = '50% ' + yPos + 'px';
+
+			$bgobj.css({ backgroundPosition: coords });
+		})
+	})
+})
